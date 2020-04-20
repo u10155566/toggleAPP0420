d1d34eb7df0aa07b5fe93c5d396e5a320f564350
u1065206 <u10155566@gmail.com>
2020-04-20 11:59:05 +0800

    開關程式完成

--- app/src/main/java/com/u1065206/toggleapp/MainActivity.java
+++ app/src/main/java/com/u1065206/toggleapp/MainActivity.java
@@ -1,13 +1,47 @@
 package com.u1065206.toggleapp;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    private ImageView img_bolb_on , img_bolb_off;
+    private ConstraintLayout constraintlayout;
+    private ToggleButton togglebutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img_bolb_on=findViewById(R.id.img_bolb_on);
+        img_bolb_off=findViewById(R.id.img_bolb_off);
+        constraintlayout=findViewById(R.id.layout);
+        togglebutton=findViewById(R.id.toggleButton);
+
+        togglebutton.setOnCheckedChangeListener(this);
+        constraintlayout.setBackgroundColor(Color.parseColor("#FFFF33"));
+        img_bolb_off.setVisibility(View.INVISIBLE);
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        if(isChecked) {
+            constraintlayout.setBackgroundColor(Color.parseColor("#000000"));
+            img_bolb_off.setVisibility(View.VISIBLE);
+            img_bolb_on.setVisibility(View.INVISIBLE);
+        }else{
+            constraintlayout.setBackgroundColor(Color.parseColor("#FFFF33"));
+            img_bolb_off.setVisibility(View.INVISIBLE);
+            img_bolb_on.setVisibility(View.VISIBLE);
+        }
     }
 }
