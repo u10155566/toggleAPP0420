d4983ace47eb421fa0ac2b974c4c786c93aa2c8a
U1065206 <u10155566@gmail.com>
2020-04-27 11:08:08 +0800

    補說明

--- app/src/main/java/com/u1065206/toggleapp/MainActivity.java
+++ app/src/main/java/com/u1065206/toggleapp/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
+    //建立各屬性
     private ImageView img_bolb_on , img_bolb_off;
     private ConstraintLayout constraintlayout;
     private ToggleButton togglebutton;
@@ -20,11 +21,11 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //取得各屬性的值，設定背景顏色，隱藏關燈圖片
         img_bolb_on=findViewById(R.id.img_bolb_on);
         img_bolb_off=findViewById(R.id.img_bolb_off);
         constraintlayout=findViewById(R.id.layout);
         togglebutton=findViewById(R.id.toggleButton);
-
         togglebutton.setOnCheckedChangeListener(this);
         constraintlayout.setBackgroundColor(Color.parseColor("#FFFF33"));
         img_bolb_off.setVisibility(View.INVISIBLE);
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
+        //當按鈕按下後檢查checked狀態變為勾，設定背景為黑色，顯示關燈圖，隱藏開燈圖_反之亦然
         if(isChecked) {
             constraintlayout.setBackgroundColor(Color.parseColor("#000000"));
             img_bolb_off.setVisibility(View.VISIBLE);
